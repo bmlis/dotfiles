9801702a61a929c59771d94f9ca46cb9c527a068
Bartlomiej Lisiecki <b.m.lisiecki@gmail.com>
2015-11-05 00:03:42 +0100

    Move scripts to separate folder, uncomment clone

--- scripts/setup_git_repos.sh
+++ scripts/setup_git_repos.sh
@@ -1,8 +1,8 @@
 echo '...cloning gnome terminal colors'
-#git clone https://github.com/gnumoksha/gnome-terminal-colors ~/Customization/gnome-terminal-colors
+git clone https://github.com/gnumoksha/gnome-terminal-colors ~/Customization/gnome-terminal-colors
 
 echo '...cloning vundle'
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo '...cloning powerline fonts'
 git clone https://github.com/powerline/fonts ~/Customization/fonts;
