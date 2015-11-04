d66845253d80c5f8e854645638ea1200cac8b121
Bartlomiej Lisiecki <b.m.lisiecki@gmail.com>
2015-11-04 23:58:38 +0100

    Setup basic repos from github (gnome themes, vundle, powerline fonts)

---
+++ setup_git_repos.sh
@@ -0,0 +1,13 @@
+echo '...cloning gnome terminal colors'
+#git clone https://github.com/gnumoksha/gnome-terminal-colors ~/Customization/gnome-terminal-colors
+
+echo '...cloning vundle'
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo '...cloning powerline fonts'
+git clone https://github.com/powerline/fonts ~/Customization/fonts;
+echo '...installing powerline fonts'
+cd ~/Customization/fonts;./install.sh;
+
+echo '...finished'
+
