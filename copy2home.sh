54ec9bcaf81a659dfb086661e2a7bffc07789922
Bartlomiej Lisiecki <b.m.lisiecki@gmail.com>
2015-11-04 23:40:46 +0100

    Scripts for copying dotfiles and utf8 in tmux

---
+++ copy2home.sh
@@ -0,0 +1,8 @@
+echo '...copying dot.files to home directory'
+
+cp ./.zshrc ~/.
+cp ./.gitconfig ~/.
+cp ./.vimrc ~/.
+cp ./.tmux.conf ~/.
+
+echo '...finished'
---
+++ copy2repo.sh
@@ -0,0 +1,8 @@
+echo '...copying dot.files from home directory'
+
+cp ~/.zshrc ./.
+cp ~/.gitconfig ./.
+cp ~/.vimrc ./.
+cp ~/.tmux.conf ./.
+
+echo '...finished'
